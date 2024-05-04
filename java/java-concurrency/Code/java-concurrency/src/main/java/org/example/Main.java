6755477382eb24c79f38140fe8d68ef25f78fa18
zainatgit <zainakhtar70@gmail.com>
2024-05-04 23:05:28 +0530

    L5

--- java/java-concurrency/Code/java-concurrency/src/main/java/org/example/Main.java
+++ java/java-concurrency/Code/java-concurrency/src/main/java/org/example/Main.java
@@ -1,7 +1,11 @@
 package org.example;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Main {
     public static void main(String[] args) {
+        AtomicInteger integer = new AtomicInteger(0);
+        integer.incrementAndGet();
         System.out.println("Hello world!");
     }
 }
